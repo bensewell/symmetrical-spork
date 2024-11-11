0929489f37cf2d12ec874077c443dfea00db338e
Ben Sewell <ben@calc-i.com>
2024-11-11 11:45:31 +0000

    Change test to expect Pelican host

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('http://localhost:8000/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/A Reliable Site/);
+  await expect(page).toHaveTitle(/A Reliable Website/);
 });
